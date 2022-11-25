bee524c28f3737e95d55c6b0f2551e82900d11d1
Nihar Kashyap <nihar1kashyap7@gmail.com>
2022-11-25 23:23:03 +0530

    Add task done

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
+var task = ["buy socks", "practise with nodejs"];
+
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index',{ task: task });
 });
 
 app.get('/greet/:name', function(req,res){
     res.send(`Hello ${req.params['name']}`)
 });
-
+app.post('/addtask', function (req, res) {
+  var newTask = req.body.newtask;
+  task.push(newTask);
+  //after adding to the array go back to the root route
+  res.redirect("/");
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')

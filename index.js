79c821e2eee0f036314bf9e39d111de7581feb4c
Nihar Kashyap <nihar1kashyap7@gmail.com>
2022-11-26 19:04:13 +0530

    Remove task completed

--- index.js
+++ index.js
@@ -6,18 +6,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 var task = ["buy socks", "practise with nodejs"];
+var complete = ["finish jquery"];
 
 
 app.get('/', function (req, res) {
-  res.render('index',{ task: task });
+  res.render('index',{ task: task,complete: complete });
 });
 
 app.get('/greet/:name', function(req,res){
     res.send(`Hello ${req.params['name']}`)
 });
+//Add task
 app.post('/addtask', function (req, res) {
   var newTask = req.body.newtask;
   task.push(newTask);
+  //after adding to the array go back to the root route
+  res.redirect("/");
+});
+
+//remove task
+app.post('/removetask', function (req, res) {
+  var completeTask = req.body.check;
+  if (typeof completeTask=="string")
+  {
+    complete.push(completeTask);
+    task.splice(task.indexOf(completeTask),1);
+  }
+  else if (typeof completeTask=="object")
+  {
+    for(var i=0;i<completeTask.length;i++)
+    {
+      complete.push(completeTask[i]);
+      task.splice(task.indexOf(completeTask[i]),1)
+    }
+  }
   //after adding to the array go back to the root route
   res.redirect("/");
 });

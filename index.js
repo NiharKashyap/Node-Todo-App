7180506c5e66019c174cee742984e281dd96e7a2
Nihar Kashyap <nihar1kashyap7@gmail.com>
2023-03-09 00:53:51 +0530

    New app started

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
@@ -10,38 +14,36 @@ var complete = ["finish jquery"];
 
 
 app.get('/', function (req, res) {
-  res.render('index',{ task: task,complete: complete });
+  res.render('index', {
+    task: task,
+    complete: complete
+  });
 });
 
-app.get('/greet/:name', function(req,res){
-    res.send(`Hello ${req.params['name']}`)
+app.get('/greet/:name', function (req, res) {
+  res.send(`Hello ${req.params['name']}`)
 });
 
 //Add task
 app.post('/addtask', function (req, res) {
   var newTask = req.body.newtask;
-  if(newTask.length!=0)
-  {
+  if (newTask.length != 0) {
     task.push(newTask);
     res.redirect("/");
   }
-  
+
 });
 
 //remove task
 app.post('/removetask', function (req, res) {
   var completeTask = req.body.check;
-  if (typeof completeTask=="string")
-  {
+  if (typeof completeTask == "string") {
     complete.push(completeTask);
-    task.splice(task.indexOf(completeTask),1);
-  }
-  else if (typeof completeTask=="object")
-  {
-    for(var i=0;i<completeTask.length;i++)
-    {
+    task.splice(task.indexOf(completeTask), 1);
+  } else if (typeof completeTask == "object") {
+    for (var i = 0; i < completeTask.length; i++) {
       complete.push(completeTask[i]);
-      task.splice(task.indexOf(completeTask[i]),1)
+      task.splice(task.indexOf(completeTask[i]), 1)
     }
   }
   //after adding to the array go back to the root route

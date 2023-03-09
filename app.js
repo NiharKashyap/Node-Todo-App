4b1bf0ee8df06cd2b34dbf6304c282fa00a75a1e
Nihar Kashyap <nihar1kashyap7@gmail.com>
2023-03-10 00:12:24 +0530

    Mongo connection added

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const dotenv = require('dotenv')
 const product = require('./routes/product.route'); // Imports routes for the products
 
+dotenv.config()
+// Set up mongoose connection
+const mongoose = require('mongoose');
+// let dev_db_url = ;
+console.log(process.env.MONGO_URI)
+let mongoDB = process.env.MONGO_URI;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // initialize our express app
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/products', product);
+
 
 
 let port = 1234;
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,3 +4,17 @@ const Product = require('../models/product.model');
 exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
+
+
+exports.product_create = function (req, res) {
+    console.log(req.body)
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+   const result = product.save()
+   res.send('Saved successfully')
+};
